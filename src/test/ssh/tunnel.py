1c29b2ae2e6fdc9111117a062d3701f204232514
Akshay Upadhayay <akshay.upadhayay@gmail.com>
2020-06-16 11:20:20 +0800

    sshforwarder

--- src/test/ssh/tunnel.py
+++ src/test/ssh/tunnel.py
@@ -1,18 +1,19 @@
-import logging
+from sshtunnel import SSHTunnelForwarder
+import requests
+import paramiko
 
+pkey = paramiko.RSAKey.from_private_key_file(private_key_path , password = private_key_password)
 
-class tunnel:
+server = SSHTunnelForwarder(
+    (remote_host, ssh_port),
+    ssh_username=remote_user,
+    ssh_private_key=pkey,
+    remote_bind_address=('localtest.com', 8080),
+    local_bind_address=('localhost', 80)
+)
 
-    def __init__(self, host, username, key_file, remote_port, host_port=nat_ssh_port_forwarding):
-        logger = logging.getLogger('sshtunnel')
-        logger.setLevel(logging.ERROR)
-    
-        try:
-            self._server = SSHTunnelForwarder((host, host_port),
-                                              ssh_username=username, ssh_private_key=key_file,
-                                              remote_bind_address=('127.0.0.1', remote_port), logger=logger)
-        except sshtunnel.BaseSSHTunnelForwarderError as e:
-            raise self.TunnelException(e)
-
-    def TunnelException(self, e):
-        pass
+server.start()
+try:
+    r = requests.get(url = 'https://localtest:80/some/endpoint', verify=False)
+finally:
+    server.stop()
